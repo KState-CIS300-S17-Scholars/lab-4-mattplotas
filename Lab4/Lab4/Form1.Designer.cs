fae2dba9bd43e7cf218b2f1579e7398121b1aa44
Matthew Plotas <plotas@win2.cs.ksu.edu>
2017-02-10 13:29:18 -0600

    finished lab

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.uxBoundBox = new System.Windows.Forms.TextBox();
+            this.uxResultBox = new System.Windows.Forms.TextBox();
+            this.uxFindButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter positive bound:";
+            // 
+            // uxBoundBox
+            // 
+            this.uxBoundBox.Location = new System.Drawing.Point(125, 18);
+            this.uxBoundBox.Name = "uxBoundBox";
+            this.uxBoundBox.Size = new System.Drawing.Size(100, 20);
+            this.uxBoundBox.TabIndex = 1;
+            // 
+            // uxResultBox
+            // 
+            this.uxResultBox.Location = new System.Drawing.Point(15, 82);
+            this.uxResultBox.Multiline = true;
+            this.uxResultBox.Name = "uxResultBox";
+            this.uxResultBox.ReadOnly = true;
+            this.uxResultBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxResultBox.Size = new System.Drawing.Size(210, 97);
+            this.uxResultBox.TabIndex = 2;
+            // 
+            // uxFindButton
+            // 
+            this.uxFindButton.Location = new System.Drawing.Point(78, 50);
+            this.uxFindButton.Name = "uxFindButton";
+            this.uxFindButton.Size = new System.Drawing.Size(75, 23);
+            this.uxFindButton.TabIndex = 3;
+            this.uxFindButton.Text = "Find Primes";
+            this.uxFindButton.UseVisualStyleBackColor = true;
+            this.uxFindButton.Click += new System.EventHandler(this.uxFindButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 197);
+            this.Controls.Add(this.uxFindButton);
+            this.Controls.Add(this.uxResultBox);
+            this.Controls.Add(this.uxBoundBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox uxBoundBox;
+        private System.Windows.Forms.TextBox uxResultBox;
+        private System.Windows.Forms.Button uxFindButton;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uxFindButton_Click(object sender, EventArgs e)
+        {
+            
+            SieveList sl = new SieveList();
+            try
+            {
+                int bound = Convert.ToInt32(uxBoundBox.Text);
+                sl.BuildList(bound);
+                if (bound >= 2)
+                {
+                    sl.FindPrimes();
+                    Console.WriteLine(sl);
+                    StringBuilder sb = new StringBuilder();
+                    foreach (int num in sl)
+                    {
+                        sb.Append(num + " ");
+                    }
+                    uxResultBox.Text = sb.ToString();
+                }else
+                {
+                    uxResultBox.Text = "";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+    }
+
+    public class SieveList : IEnumerable<int>
+    {
+        private Node head, tail;
+
+        public SieveList(){ }
+
+        public void BuildList(int bound)
+        {
+            Node temp = head = new Node(2, null);
+            for (int i = 3; i < bound; i++){
+                temp.Next = new Node(i, null);
+                temp = temp.Next;
+            }
+            tail = temp.Next = new Node(bound, null);
+        }
+
+        public override string ToString()
+        {
+            return head.Data + " " + tail.Data;
+        }
+        public void FindPrimes()
+        {
+            Node divisor, dividend, previous;
+            divisor = previous = head;
+            while (divisor.Data <= Math.Sqrt(tail.Data))
+            {
+                dividend = divisor.Next;
+                while(dividend != null)
+                {
+                    if (dividend.Data % divisor.Data == 0)
+                        previous.Next = dividend.Next;
+                    previous = dividend;
+                    dividend = dividend.Next;
+                }
+                divisor = divisor.Next;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new SieveEnumerator(head);
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new SieveEnumerator(head);
+            throw new NotImplementedException();
+        }
+
+        private class Node
+        {
+            private int data;
+            private Node next;
+
+            public Node(int data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+
+            public int Data
+            {
+                get
+                {
+                    return data;
+                }
+                set
+                {
+                    data = value;
+                }
+            }
+
+            public Node Next
+            {
+                get
+                {
+                    return next;
+                }
+                set
+                {
+                    next = value;
+                }
+            }
+        }
+
+        private class SieveEnumerator : IEnumerator<int>
+        {
+
+            private Node currentNode;
+            public SieveEnumerator(Node head)
+            {
+                Node dummy = new Node(0, head);
+                currentNode = dummy;
+            }
+
+            public int Current
+            {
+                get
+                {
+                    return currentNode.Data;
+                    //throw new NotImplementedException();
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return currentNode.Data;
+                    //throw new NotImplementedException();
+                }
+            }
+
+            public void Dispose()
+            {
+                //throw new NotImplementedException();
+            }
+
+            public bool MoveNext()
+            {
+                if (currentNode.Next == null)
+                    return false;
+                currentNode = currentNode.Next;
+                return true;
+                //throw new NotImplementedException();
+            }
+
+            public void Reset()
+            {
+                //throw new NotImplementedException();
+            }
+        }
+    }
+}
